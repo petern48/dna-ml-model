e5fa5ab007e3cc666d58f944cb63b557664ecbc0
Peter Nguyen <petern0408@gmail.com>
2024-01-03 05:00:00 -0800

    Rework dna_dataset so data is loaded before inputting into dataset, remove TestDataset class

--- dna_dataset.py
+++ dna_dataset.py
@@ -1,128 +1,106 @@
 import torch
 import random
-from constants import *
+
 import sys
 from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
 import numpy as np
 # import utils
 import pandas as pd
 
+
+LINES_PER_SEQUENCE = 4
+ACCESSIBLE_LABEL = 1
+NOT_ACCESSIBLE_LABEL = 0
+
 rand = random.Random(1)
-
-# Felt lazy so i copy and pasted rather than reworked the dataset for inheritance
-class TestDataset(torch.utils.data.Dataset):
-    def __init__(self, data_path):
-        self.sequences = []
-        self.ids = []
-
-        bases = ["A", "C", "G", "T"]
-        self.lb = LabelBinarizer()
-        # self.lb = OneHotEncoder
-        self.lb.fit_transform(bases)
-
-        count = self.read_data_file(data_path)
-
-        assert(count == len(self.ids) and count == len(self.sequences))
-
-    def read_data_file(self, data_file):
-        seq_count = 0
-        with open(data_file, "r") as f:
-            while True:
-                try:
-                    id = next(f)[1:].rstrip()   # accessible47239  or seq1
-                except StopIteration:
-                    break
-                seq_count += 1
-                sequence = ""
-
-                for _ in range(LINES_PER_SEQUENCE):  # read the 4 lines of dna sequence
-                    sequence += next(f).rstrip()  # Collapse to one sequence
-
-                sequence = self.lb.transform(list(sequence))  # input a string sequence
-                sequence = np.transpose(sequence)
-
-                self.sequences.append(torch.Tensor(sequence))  # rstrip()
-                self.ids.append(id)
-        return seq_count
+bases = ["A", "C", "G", "T"]
+lb = LabelBinarizer()
+lb.fit_transform(bases)
 
 
-    def __getitem__(self, i):
-        """Gets the ith sequence from the dataset"""
-        batch = {
-            "sequences": self.sequences[i],
-            "ids": self.ids[i]
-        }
-        return batch
+def read_data_file(data_file, accessible=True, labeled=True, shuffle=True):
 
-    def __len__(self):
-        return len(self.sequences)
+    label = ACCESSIBLE_LABEL if accessible else NOT_ACCESSIBLE_LABEL
+    if not labeled:
+        label = None
+
+    sequences = []
+    labels = []
+    ids = []
+
+    with open(data_file, "r") as f:
+        while True:
+            try:
+                id = next(f)[1:].rstrip()   # accessible47239  or seq1
+            except StopIteration:
+                break
+            sequence = ""
+
+            for _ in range(LINES_PER_SEQUENCE):  # read the 4 lines of dna sequence
+                sequence += next(f).rstrip()  # Collapse to one sequence
+
+            sequence = lb.transform(list(sequence))  # input a string sequence
+            sequence = np.transpose(sequence)
+
+            sequences.append(torch.Tensor(sequence))
+
+            if labeled != None:
+                labels.append(label)
+
+            ids.append(id)
+
+    if shuffle:
+        if labeled:
+            sequences, labels = shuffle_lists(sequences, labels)
+        else:
+            sequences, ids = shuffle_lists(sequences, ids)
+
+    return sequences, labels, ids
+
+
+def shuffle_lists(list1, list2):
+    """Shuffle the two input lists together as one unit"""
+    zipped = list(zip(list1, list2))
+    rand.shuffle(zipped)
+    list1, list2 = zip(*zipped)
+    return list(list1), list(list2)
 
 
 class DNADataset(torch.utils.data.Dataset):
-    def __init__(self, acc_data_path, not_acc_data_path):
-        self.accessible_count = 0
-        self.not_accessible_count = 0
-        self.sequences = []  # convert to regular lists later? not self.list
-        self.labels = []
+    def __init__(self, sequences, labels, ids=None):
+        """Input lists are shuffled"""
 
-        bases = ["A", "C", "G", "T"]
-        self.lb = LabelBinarizer()
-        self.lb.fit_transform(bases)
+        if ids == None:
+            sequences, labels = shuffle_lists(sequences, labels)
+            ids = [0] * len(sequences)
 
-        self.accessible_count = self.read_data_file(acc_data_path, accessible=True)
-        self.not_accessible_count = self.read_data_file(not_acc_data_path, accessible=False)
+        if labels == None:
+            sequences, ids = shuffle_lists(sequences, ids)
+            labels = [0] * len(sequences)
 
-        assert(self.accessible_count + self.not_accessible_count == len(self.sequences))
+        assert(len(sequences) == len(labels) == len(ids))
 
-        self.sequences, self.labels = self.shuffle_lists(self.sequences, self.labels)
-
-        self.df = pd.DateFrame({
-            "sequences":self.sequences,
-            "labels":self.labels
+        # self.accessible_count = 0
+        # self.not_accessible_count = 0
+        # self.accessible_count = self.read_data_file(acc_data_path, accessible=True)
+        # self.not_accessible_count = self.read_data_file(not_acc_data_path, accessible=False)
+        # assert(self.accessible_count + self.not_accessible_count == len(self.sequences))
+        # self.sequences, self.labels = self.shuffle_lists(self.sequences, self.labels)
+        self.df = pd.DataFrame({
+            "sequences": sequences,
+            "labels": labels,
+            "ids": ids
         })
-
-
-    def read_data_file(self, data_file, accessible=True):
-
-        label = ACCESSIBLE_LABEL if accessible else NOT_ACCESSIBLE_LABEL
-        seq_count = 0
-
-        with open(data_file, "r") as f:
-            while True:
-                try:
-                    id = next(f)[1:].rstrip()   # accessible47239  or seq1
-                except StopIteration:
-                    break
-                seq_count += 1
-                sequence = ""
-
-                for _ in range(LINES_PER_SEQUENCE):  # read the 4 lines of dna sequence
-                    sequence += next(f).rstrip()  # Collapse to one sequence
-
-                sequence = self.lb.transform(list(sequence))  # input a string sequence
-                sequence = np.transpose(sequence)
-                # append the sequence and label
-
-                self.sequences.append(torch.Tensor(sequence))
-                self.labels.append(label)
-                # self.ids.append(id)
-
-        return seq_count
-
-
-    def shuffle_lists(self, list1, list2):
-        zipped = list(zip(list1, list2))
-        rand.shuffle(zipped)
-        list1, list2 = zip(*zipped)
-        return list(list1), list(list2)
 
 
     def __getitem__(self, i):
         """Gets the ith sequence from the dataset"""
         row = self.df.iloc[i]
+
         batch = {
             "sequences": row["sequences"],
-            "labels": row["labels"]
+            "labels": row["labels"],
+            "ids": row["ids"]
         }
         return batch
 
@@ -133,5 +111,5 @@ class DNADataset(torch.utils.data.Dataset):
 
 
 # For testing can just run python dna_dataset.py
-if __name__ == "__main__":
-    DNADataset(TEST_FILE, TEST_FILE)
+# if __name__ == "__main__":
+#     DNADataset(TEST_FILE, TEST_FILE)

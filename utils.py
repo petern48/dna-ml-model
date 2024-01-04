704e4157354f301c39c7a5570a81b77d836a5bc1
Peter Nguyen <petern0408@gmail.com>
2024-01-03 18:07:19 -0800

    Add compute_metrics function for f1 score, precision, etc. Call it in the evaluate()

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import itertools
 from gensim.models import Word2Vec
 # from torcheval.metrics.functional import multiclass_f1_score
 from torchmetrics.functional import f1_score
+from sklearn.metrics import confusion_matrix
 
 
 # not in use
@@ -29,10 +30,24 @@ def get_kmers(sequence, k=6):
     # working, returns list of substrings
 
 
+def compute_metrics(CM):
+    """
+    https://www.kaggle.com/code/ajinkyaabhang/implementing-acc-precision-recall-f1-from-scratch/notebook
+    """
+    # true negative, ... false positive, etc
+    tn, tp, fp, fn = CM[0][0], CM[1][1], CM[0][1], CM[1][0]
+    acc_score = (tp + tn) / (tp + tn + fp + fn)
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1 = 2 * precision * recall / (precision + recall)
+
+    return acc_score, precision, recall, f1
+
+
+# Not in use
 def compute_accuracy(outputs, labels):
     """
     Computes the accuracy of a model's predictions.
-
     Example input:
         outputs: [0.7, 0.9, 0.3, 0.2]
         labels:  [1, 1, 0, 1]
@@ -54,6 +69,7 @@ def evaluate(val_loader, model, loss_fn, device):
     n_correct = 0.0
     total_outputs = torch.empty(0).to(device)
     total_labels = torch.empty(0).to(device)
+    CM = 0
 
     for batch in val_loader:
         val_samples, val_labels = batch['sequence'].to(device), batch['label'].to(device)
@@ -66,14 +82,18 @@ def evaluate(val_loader, model, loss_fn, device):
         total_outputs = torch.cat((total_outputs, outputs))
         total_labels = torch.cat((total_labels, val_labels))
 
+        CM += confusion_matrix(val_labels.flatten(), torch.round(outputs).flatten())
+
+        acc_score, precision, recall, f1 = compute_metrics(CM)
+
         total_loss += loss_fn(outputs, val_labels).item()  # change tensor to single val
         n_correct += (torch.round(outputs) == val_labels).sum().item()
         n_total += len(outputs)
 
-    val_accuracy = n_correct / n_total
+    accuracy = n_correct / n_total
 
-    # f1_score = multiclass_f1_score(total_outputs.flatten(), total_labels.flatten())  # f1 score outputs all 0s
-    f1 = f1_score(total_outputs.flatten(), total_labels.flatten(), task="binary", num_classes=2).item()
+    f1_2 = f1_score(total_outputs.flatten(), total_labels.flatten(), task="binary", num_classes=2).item()
+    assert(acc_score == accuracy)
+    assert(round(f1, 3) == round(f1_2, 3))
 
-    return total_loss, val_accuracy, f1
-
+    return total_loss, accuracy, precision, recall, f1
